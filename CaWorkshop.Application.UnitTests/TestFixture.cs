0afd0e4ddfd2056e28fe2864e2c357cbfd7de03a
Jason Taylor <jasongt@outlook.com>
2020-07-28 14:18:03 +1000

    BeyondBasics-7

---
+++ CaWorkshop.Application.UnitTests/TestFixture.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using CaWorkshop.Infrastructure.Persistence;
+using System;
+
+namespace CaWorkshop.Application.UnitTests
+{
+    public class TestFixture : IDisposable
+    {
+        // Setup 
+        public TestFixture()
+        {
+            Context = DbContextFactory.Create();
+            Mapper = MapperFactory.Create();
+        }
+
+        public ApplicationDbContext Context { get; }
+
+        public IMapper Mapper { get; }
+
+        // Cleanup
+        public void Dispose()
+        {
+            DbContextFactory.Destroy(Context);
+        }
+    }
+}
---
+++ CaWorkshop.Application.UnitTests/TodoLists/Commands/CreateTodoList/CreateTodoListCommandTests.cs
@@ -0,0 +1,41 @@
+﻿using CaWorkshop.Application.TodoLists.Commands.CreateTodoList;
+using CaWorkshop.Infrastructure.Persistence;
+using Shouldly;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CaWorkshop.Application.UnitTests.TodoLists.Commands.CreateTodoList
+{
+    public class CreateTodoListCommandTests : TestFixture
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateTodoListCommandTests()
+        {
+            _context = base.Context;
+        }
+
+        [Fact]
+        public async Task Handle_ShouldPersistTodoList()
+        {
+            // Arrange
+            var command = new CreateTodoListCommand
+            {
+                Title = "Bucket List"
+            };
+
+            var handler = new CreateTodoListCommandHandler(_context);
+
+            // Act
+            var id = await handler.Handle(command,
+                CancellationToken.None);
+
+            // Assert
+            var entity = _context.TodoLists.Find(id);
+
+            entity.ShouldNotBeNull();
+            entity.Title.ShouldBe(command.Title);
+        }
+    }
+}
