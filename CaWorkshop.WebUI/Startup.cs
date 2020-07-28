cd95989b34535b4285cf330ba7d7f64a5735b643
Jason Taylor <jasongt@outlook.com>
2020-07-28 12:06:45 +1000

    BeyondBasics-6

--- CaWorkshop.WebUI/Startup.cs
+++ CaWorkshop.WebUI/Startup.cs
@@ -1,5 +1,6 @@
 using CaWorkshop.Application;
 using CaWorkshop.Infrastructure;
+using CaWorkshop.WebUI.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,8 +24,11 @@ namespace CaWorkshop.WebUI
             services.AddInfrastructureServices(Configuration);
             services.AddApplicationServices();
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(options =>
+                options.Filters.Add(new ApiExceptionFilter()));
+
             services.AddRazorPages();
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
